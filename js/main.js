9356d82656f2d6403b56f5fcceb7ab6910c70500
ali roshan nia <ali.roshan.nia.1379@gmail.com>
2021-09-29 13:32:07 +0330

    finish section 1 with responsiving

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-function getMin(array) {
-    let min = Number.MAX_VALUE;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] <= min) {
-            min = array[i];
-        }
-    }
-    return min;
+function alert() {
+    console.log('salam');
 }
-
-// (1,2,3,4,5,)
-// (5,4,3,2,1)
-
-
-// g(n) = n-1
